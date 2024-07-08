ab498432a7c751c609bc1a0f5e70c5c07ed35c93
ANDRE MARTI PEREZ TORRES <andre.perez@pjenl.gob.mx>
2024-07-08 17:24:02 -0600

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,21 @@ import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import Providers from "@/components/providers";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "LeoGPT",
+  description: "Asistente inteligente especializado en la UMM",
+  manifest: "/manifest.json",
+  icons: {
+    apple: "/icon.png",
+  },
+}
 
 interface RootLayoutProps {
   children: React.ReactNode;
